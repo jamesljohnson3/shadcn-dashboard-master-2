d3794b884e90a24d58873bb7703065d4b784abcd
Admin <support@unlimitpotential.com>
2024-01-18 18:52:08 -0500

    s

--- next.config.js
+++ next.config.js
@@ -1,4 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    compiler: {
+      removeConsole: true,
+    },
+    reactStrictMode: true,
+    experimental: {
+      serverComponentsExternalPackages: ['@evervault/sdk'],
+      esmExternals: 'loose',
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'img.clerk.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.clerk.dev',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'hjhncoqotxlxpvrljjgz.supabase.co',
+        },
+        {
+          protocol: "https",
+          hostname: "image.tmdb.org",
+          port: "",
+        },
+      ],
+    },
+   
+  };
+  
+  module.exports = nextConfig;
+  
