23007439fae2d2c528f5d5e4dcb01c1fd2f1a252
Admin <support@unlimitpotential.com>
2024-01-18 20:11:18 -0500

    gf

--- app/(root)/(auth)/layout.tsx
+++ app/(root)/(auth)/layout.tsx
@@ -6,13 +6,9 @@ import { authOptions } from "@/app/utils/auth";
 import { redirect } from "next/navigation";
 
 export default async function AuthLayout({ children }: { children: ReactNode }) {
-  const session = await getServerSession(authOptions);
-  if (session) {
-    return redirect("/home");
-  } else {
+
     return     <div className=" justify-center p-6 items-center ">
      
     {children}
   </div>
-}
 }
