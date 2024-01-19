e39e5b3b8bc1c831119c4257094fccea223943b0
Admin <support@unlimitpotential.com>
2024-01-18 21:09:48 -0500

    s

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,47 @@
 import { ReactNode } from "react";
- 
+import { auth } from "@clerk/nextjs";
+import DashNavbar from "@/components/Navbar";
+import NextTopLoader from "nextjs-toploader";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (     
+  const { userId } = auth();
+  console.log("Fetched userId:", userId);
 
-  <> 
+  if (!userId) {
+    return (
+      <div className="flex-grow mt-6 w-full md:overflow-y-auto sm:p-6">
         {children}
-      </>
+      </div>
+    );
+  }
 
+  return (
+    <>
+      <div className="flex flex-grow flex-col min-h-screen min-w-full bg-background">
+        <NextTopLoader
+          color="#7B00D3"
+          initialPosition={0.78}
+          crawlSpeed={800}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={400}
+          shadow="0 0 10px #7B00D3,0 0 5px #7B00D3"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+          <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />
+
+        <DashNavbar params={{
+          storeId: userId
+        }} />
+
+        <div className="flex h-screen relative flex-col md:flex-row md:overflow-hidden">
+          {children}
+        </div>
+      </div>
+    </>
   );
 }
