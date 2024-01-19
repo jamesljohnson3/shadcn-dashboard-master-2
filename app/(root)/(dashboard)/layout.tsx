8094ea4ae98c9efb8d6b8823158d7415e42d7ff7
Admin <support@unlimitpotential.com>
2024-01-18 19:46:58 -0500

    s

--- app/(root)/(dashboard)/layout.tsx
+++ app/(root)/(dashboard)/layout.tsx
@@ -3,6 +3,8 @@ import * as React from "react"
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/utils/auth";
 import { redirect } from "next/navigation";
+import { cn } from "@/lib/utils";
+import SideNavbar from "@/components/SideNavbar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -16,6 +18,23 @@ export default async function DashboardLayout({
   if (!session) {
     return redirect("/login");
   } else {
-    return    <> {children}</>
+    return    <> <div
+    className={cn(
+      "min-h-screen w-full bg-white text-black flex ",
+      {
+        "debug-screens": process.env.NODE_ENV === "development"
+      }
+    )}
+  >
+    {/* sidebar */}
+    {/* <p className="border">Sidebar</p> */}
+    <SideNavbar />
+    {/* main page */}
+    <div className="p-8 w-full">{children}</div>
+  </div></>
 }
 }
+
+
+
+/** @format */
