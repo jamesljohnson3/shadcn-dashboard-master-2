e4acc5d5ad5ebec30105d0c8993f4d33ee000dad
Admin <support@unlimitpotential.com>
2024-01-18 21:34:40 -0500

    w

--- app/(root)/(dashboard)/store/[storeId]/(routes)/settings/page.tsx
+++ app/(root)/(dashboard)/store/[storeId]/(routes)/settings/page.tsx
@@ -1,12 +1,9 @@
 
-import  Radix1  from "@/components/dashboard"
-
-import  Radix2  from "@/components/cards"
 
 export default function AuthenticationPage() {
   return (
     <>
- <Radix1/> <Radix2/>
+Settings Page
     </>
   )
 }
