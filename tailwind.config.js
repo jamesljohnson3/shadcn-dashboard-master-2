bd16f519da80d1f34ae77e86222218915bf66146
Admin <support@unlimitpotential.com>
2024-01-18 22:54:46 -0500

    y

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ import {nextui} from "@nextui-org/react";
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   prefix: "",
   theme: {
