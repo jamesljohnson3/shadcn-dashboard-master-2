285c4087f8fdc069f60adcd7aefba854b7c58d59
Admin <support@unlimitpotential.com>
2024-01-18 20:02:13 -0500

    f

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { PricingSection } from "@/app/components/sections/pricing-section"
+import { TestimonialsSection } from "@/app/components/sections/testimonials-section"
+
+export default function LandingPage(): JSX.Element {
+  return (
+
+  
+      <><TestimonialsSection /><div className="mt-32">
+      <PricingSection />
+
+    </div></>
+  )
+}
