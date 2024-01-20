89be71208aa2594cbf8ffab944732c323e4d5e78
Admin <support@unlimitpotential.com>
2024-01-20 18:10:57 -0500

    s

--- components/SideNavbar.tsx
+++ components/SideNavbar.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 
 "use client"
+/** @format */
+
 import React, { useState }from 'react';
 
 import { AppShell,  } from '@saas-ui/app-shell'
@@ -14,6 +16,70 @@ import {
   import ThemeToggle from "../components/layouts/theme-toggle-white";
   import { IconButton, Spacer, Image, Heading, HStack } from "@chakra-ui/react";
   import { Popover } from '@headlessui/react'
+import { Nav } from "./ui/nav";
+
+type Props = {};
+
+import {
+  ShoppingCart,
+  LayoutDashboard,
+  Settings,
+  ChevronRight
+} from "lucide-react";
+import { Button } from "./ui/button";
+
+import { useWindowWidth } from "@react-hook/window-size";
+
+function Links({}: Props) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const onlyWidth = useWindowWidth();
+  const mobileWidth = onlyWidth < 768;
+
+  function toggleSidebar() {
+    setIsCollapsed(!isCollapsed);
+  }
+
+  return (
+    <div className="relative min-w-[80px] border-r px-3  pb-10 pt-24 ">
+      {!mobileWidth && (
+        <div className="absolute right-[-20px] top-7">
+          <Button
+            onClick={toggleSidebar}
+            variant="secondary"
+            className=" rounded-full p-2"
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
+      <Nav
+        isCollapsed={mobileWidth ? true : isCollapsed}
+        links={[
+          {
+            title: "Dashboard",
+            href: "/",
+            icon: LayoutDashboard,
+            variant: "default"
+          },
+      
+          {
+            title: "Ordrs",
+            href: "/orders",
+            icon: ShoppingCart,
+            variant: "ghost"
+          },
+          {
+            title: "Settings",
+            href: "/settings",
+            icon: Settings,
+            variant: "ghost"
+          }
+        ]}
+      />
+    </div>
+  );
+}
 
 
  function Dashboard() {
@@ -133,6 +199,7 @@ import {
                 
 
               >
+                <Links/>
             </NavGroup>
   <NavGroup>  
     </NavGroup>
