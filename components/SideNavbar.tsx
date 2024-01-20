f6796a06d22820a8d86f2c579100fa80d1378de0
Admin <support@unlimitpotential.com>
2024-01-20 17:31:30 -0500

    r

--- components/SideNavbar.tsx
+++ components/SideNavbar.tsx
@@ -1,68 +1,84 @@
-/** @format */
 "use client";
 
-import { useState } from "react";
-import { Nav } from "./ui/nav";
-
-type Props = {};
+import {
+  Box,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+  useColorMode,
+} from "@chakra-ui/react";
+import {
+  AppShell,
+  Sidebar,
+  SidebarSection,
+  NavItem,
+  PersonaAvatar,
+  SidebarToggleButton,
+} from "@saas-ui/react";
 
 import {
-  ShoppingCart,
-  LayoutDashboard,
-  Settings,
-  ChevronRight
-} from "lucide-react";
-import { Button } from "./ui/button";
+  FiBarChart,
+  FiHome,
+  FiMoon,
+  FiSettings,
+  FiSun,
+  FiUsers,
+} from "react-icons/fi";
 
-import { useWindowWidth } from "@react-hook/window-size";
+import Image from "next/image";
 
-export default function SideNavbar({}: Props) {
-  const [isCollapsed, setIsCollapsed] = useState(false);
 
-  const onlyWidth = useWindowWidth();
-  const mobileWidth = onlyWidth < 768;
-
-  function toggleSidebar() {
-    setIsCollapsed(!isCollapsed);
-  }
+export function SidebarLayout() {
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <div className="relative min-w-[80px] border-r px-3  pb-10 pt-24 ">
-      {!mobileWidth && (
-        <div className="absolute right-[-20px] top-7">
-          <Button
-            onClick={toggleSidebar}
-            variant="secondary"
-            className=" rounded-full p-2"
-          >
-            <ChevronRight />
-          </Button>
-        </div>
-      )}
-      <Nav
-        isCollapsed={mobileWidth ? true : isCollapsed}
-        links={[
-          {
-            title: "Dashboard",
-            href: "/",
-            icon: LayoutDashboard,
-            variant: "default"
-          },
-      
-          {
-            title: "Ordrs",
-            href: "/orders",
-            icon: ShoppingCart,
-            variant: "ghost"
-          },
-          {
-            title: "Settings",
-            href: "/settings",
-            icon: Settings,
-            variant: "ghost"
-          }
-        ]}
-      />
-    </div>
+
+    <> <div 
+    style={{ maxWidth: '250px' }}
+
+    ><Sidebar
+      variant="condensed"
+      borderWidth="0"
+      height="$100vh"
+      spacing="3"
+      style={{
+        background: 'linear-gradient(to bottom, #4A2673, #331E53) 80%', // Adjust the color codes and percentage as needed
+        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)', // Add a background shadow
+        borderRadius: '8px', // Add border-radius for rounded corners
+        padding: '2px', // Add padding for content spacing
+        position: 'relative', // Ensure positioning for glow effect
+        overflow: 'hidden', // Ensure overflow behavior for glow effect
+      }}
+    >            <SidebarToggleButton />
+      <SidebarSection direction="row">
+        <Spacer />
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            icon={<PersonaAvatar presence="online" size="xs" name="John Doe" />}
+            variant="ghost" />
+          <MenuList>
+            <MenuItem>Sign out</MenuItem>
+          </MenuList>
+        </Menu>
+      </SidebarSection>
+      <SidebarSection flex="1" overflowY="auto">
+        <NavItem icon={<FiHome size="1.2em" />}>Home</NavItem>
+        <NavItem icon={<FiUsers size="1.2em" />} isActive={true}>
+          Users
+        </NavItem>
+        <NavItem icon={<FiBarChart size="1.2em" />}>Analytics</NavItem>
+        <NavItem icon={<FiSettings size="1.2em" />}>Settings</NavItem>
+      </SidebarSection>
+      <SidebarSection alignItems="flex-start">
+        <IconButton
+          icon={colorMode === "dark" ? <FiMoon /> : <FiSun />}
+          aria-label="Toggle color mode"
+          onClick={toggleColorMode} />
+      </SidebarSection>
+    </Sidebar></div></>
   );
 }
