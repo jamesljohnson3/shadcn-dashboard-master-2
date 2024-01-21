ff740f53a914014c7fb9e96e2313753c3ea8f8a7
Admin <support@unlimitpotential.com>
2024-01-21 01:31:52 -0500

    h

--- app/(root)/(dashboard)/dashboard/pub-sub/space.tsx
+++ app/(root)/(dashboard)/dashboard/pub-sub/space.tsx
@@ -2,6 +2,7 @@
 import Spaces from "@ably/spaces";
 import { SpaceProvider, SpacesProvider } from "@ably/spaces/react";
 import AvatarStack from "@/components/AvatarStack";
+import ChatSection from "./components/chat-section";
 
 const spaceName = "";
 
@@ -9,10 +10,11 @@ export default function Home({ spaces }: { spaces: Spaces }) {
 	return (  <SpacesProvider client={spaces}>
 		<SpaceProvider name={"test"}>
 		 
-		<main className="">
+		<main className="flex min-h-screen flex-col items-center gap-10 p-24 bg-gradient-to-bl from-slate-300 to-blue-50">
 			<h1 className="font-bold text-4xl">GPT4 Vision Chatbot </h1>
 			<AvatarStack />
-		
+			<ChatSection />
+
 		</main>
     </SpaceProvider>
   </SpacesProvider>
