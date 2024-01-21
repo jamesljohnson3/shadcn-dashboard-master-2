b37110ddf5f1ce32248078bfe5f63f2bbdf1663f
Admin <support@unlimitpotential.com>
2024-01-20 20:55:06 -0500

    w

--- app/(root)/(dashboard)/dashboard/chatgpt4/page.tsx
+++ app/(root)/(dashboard)/dashboard/chatgpt4/page.tsx
@@ -3,8 +3,8 @@ import Footer from "./components/footer";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center gap-10 p-24 bg-gradient-to-bl from-slate-300 to-blue-50">
-			<h1 className="font-bold text-4xl">GPT4 Vision Chatbot Demo</h1>
+		<main className="">
+			<h1 className="font-bold text-4xl">GPT4 Vision Chatbot </h1>
 			<ChatSection />
 			<Footer />
 		</main>
