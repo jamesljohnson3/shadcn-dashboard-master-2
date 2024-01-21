9d0aed51acf338bd22f9c28ea13f0ab9d44bd7bb
Admin <support@unlimitpotential.com>
2024-01-20 21:59:38 -0500

    s

--- next.config.js
+++ next.config.js
@@ -45,6 +45,11 @@ const nextConfig = {
           hostname: "image.tmdb.org",
           port: "",
         },
+        
+        {
+          protocol: 'https',
+          hostname: 'unlimitednow.live',
+        },
       ],
     },
    
