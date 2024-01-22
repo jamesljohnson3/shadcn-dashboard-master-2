3cad0bf8b796db88e0af3d62566f87b52588ad95
Admin <support@unlimitpotential.com>
2024-01-22 14:04:53 -0500

    j

--- middleware.ts
+++ middleware.ts
@@ -1,49 +1,10 @@
-
-import { NextResponse } from "next/server";
-import { authMiddleware } from "@clerk/nextjs"
-
-
-
-export default authMiddleware({
-  // routes that don't require authentication
-  publicRoutes: [
-    "/terms-conditions",
-   "/signup(.*)",
-   "/signin(.*)",
-   "/register",
-"/demo",
-"/sites(.*)",
-"/site(.*)",
-"/home(.*)",
-"/post(.*)",
-"/welcome",
- ,  "/auth",
-   "/sign-up(.*)",
- "/sign-in(.*)",   
- "/login",
- "/dashboard(.*)",
- "/p(.*)",
- "/"
-  ],
-
-  afterAuth(auth, req) {
-    if (auth.isPublicRoute) {
-      // do nothing for public routes
-      return NextResponse.next()
-    }
-
-    const url = new URL(req.nextUrl.origin)
-
-    if (!auth.userId && !auth.isPublicRoute) {
-      // if the user tries to access a private route without being authenticated,
-      // redirect to login page
-      url.pathname = "/login"
-      return NextResponse.redirect(url)
-    }
-  },
-})
-
+import { authMiddleware } from "@clerk/nextjs";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({});
+ 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/((?!api|_next/static|_next/image|.png).*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
