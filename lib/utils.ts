d35dcc6ad612606d5deb29bca03d6c032bfca1dc
Admin <support@unlimitpotential.com>
2024-01-21 20:17:03 -0500

    d

--- lib/utils.ts
+++ lib/utils.ts
@@ -127,3 +127,36 @@ export function absoluteUrl(path: string) {
 export function truncate(str: string, length: number) {
   return str.length > length ? `${str.substring(0, length)}...` : str
 }
+
+
+
+export async function chatfetcher<JSON = any>(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<JSON> {
+  const res = await fetch(input, init)
+
+  if (!res.ok) {
+    const json = await res.json()
+    if (json.error) {
+      const error = new Error(json.error) as Error & {
+        status: number
+      }
+      error.status = res.status
+      throw error
+    } else {
+      throw new Error('An unexpected error occurred')
+    }
+  }
+
+  return res.json()
+}
+
+export function formatDate(input: string | number | Date): string {
+  const date = new Date(input)
+  return date.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  })
+}
