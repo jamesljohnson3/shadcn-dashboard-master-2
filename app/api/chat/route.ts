190e2658f91193452191d98dd0ea585217091ed5
Admin <support@unlimitpotential.com>
2024-01-21 21:24:36 -0500

    f

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,39 +1,66 @@
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { kv } from '@vercel/kv'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import OpenAI from 'openai'
+
+import { currentUser } from '@clerk/nextjs';
+import { nanoid } from '@/lib/utils'
+
+export const runtime = 'edge'
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const createRequestMessages = async (req: Request) => {
-  const { messages, data } = await req.json();
-  if (!data?.imageUrl) return messages;
-
-  const initialMessages = messages.slice(0, -1);
-  const currentMessage = messages[messages.length - 1];
-  return [
-    ...initialMessages,
-    {
-      ...currentMessage,
-      content: [
-        { type: "text", text: currentMessage.content },
-        {
-          type: "image_url",
-          image_url: data.imageUrl,
-        },
-      ],
-    },
-  ];
-};
+  apiKey: process.env.OPENAI_API_KEY
+})
 
 export async function POST(req: Request) {
-  const inputMessages = await createRequestMessages(req);
-  const response = await openai.chat.completions.create({
-    model: "gpt-4-vision-preview",
-    stream: true,
-    messages: inputMessages,
-    max_tokens: 2000,
-  });
-  const stream = OpenAIStream(response);
-  return new StreamingTextResponse(stream);
+  const json = await req.json()
+  const { messages, previewToken } = json
+  const user = await currentUser();
+
+  if (!user) {
+    return new Response('Unauthorized', {
+      status: 401
+    })
+  }
+  const userId = user.id;
+
+  if (previewToken) {
+    openai.apiKey = previewToken
+  }
+
+  const res = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages,
+    temperature: 0.7,
+    stream: true
+  })
+
+  const stream = OpenAIStream(res, {
+    async onCompletion(completion) {
+      const title = json.messages[0].content.substring(0, 100)
+      const id = json.id ?? nanoid()
+      const createdAt = Date.now()
+      const path = `/chat/${id}`
+      const payload = {
+        id,
+        title,
+        userId,
+        createdAt,
+        path,
+        messages: [
+          ...messages,
+          {
+            content: completion,
+            role: 'assistant'
+          }
+        ]
+      }
+      await kv.hmset(`chat:${id}`, payload)
+      await kv.zadd(`user:chat:${userId}`, {
+        score: createdAt,
+        member: `chat:${id}`
+      })
+    }
+  })
+
+  return new StreamingTextResponse(stream)
 }
