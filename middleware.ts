33cc947bd51b9658e35f3b5f2d2fe809e0efeb50
Admin <support@unlimitpotential.com>
2024-01-22 23:03:43 -0500

    s

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,15 @@
 
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import { NextResponse } from 'next/server'
+// middleware.ts
+
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
+  publicRoutes: ["/"], // Allow access to the "/" route for all users
+  
   afterAuth(auth, req, evt) {
     // Allow all logged-in users to access every route
     if (auth.userId) {
-      return NextResponse.next();
-    }
-
-    // For non-logged-in users, allow access to "/" and "/login" routes
-    if (!auth.userId && auth.isPublicRoute && (req.url === "/" || req.url === "/login")) {
       return NextResponse.next();
     }
 
