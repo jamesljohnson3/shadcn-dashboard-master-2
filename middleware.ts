4106dc7dfea38bdae3b5d7d2e6483b8b62f164c7
Admin <support@unlimitpotential.com>
2024-01-23 00:18:20 -0500

    ,

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export default authMiddleware({
   // Specify routes that should be accessible without authentication
-  publicRoutes: ["/", "/login"],
+  publicRoutes: ["/", "/login", "/player",  "/signup(.*)",
+  "/signin(.*)"],
 
   // This function is called after the authentication middleware is executed
   afterAuth(auth, req, evt) {
@@ -14,6 +14,15 @@ export default authMiddleware({
     }
 
     // Redirect non-logged-in users to the sign-in route for other routes
-    return redirectToSignIn({ returnBackUrl: req.url });
+    if (!auth.userId && !auth.isPublicRoute) {
+      const url = new URL(req.url);
+      // if the user tries to access a private route without being authenticated,
+      // redirect to login page
+      url.pathname = "/login";
+      return NextResponse.redirect(url.toString());
+    }
+
+    // If it's a public route or any other case, proceed
+    return NextResponse.next();
   },
 });
