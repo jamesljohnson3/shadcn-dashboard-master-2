1c3eb82ed3b47e8ae1c7510d9bdf12eaa4f86a9c
Admin <support@unlimitpotential.com>
2024-01-22 22:48:38 -0500

    d

--- middleware.ts
+++ middleware.ts
@@ -1,47 +1,51 @@
-
-import { NextResponse } from "next/server";
-import { authMiddleware } from "@clerk/nextjs"
-
-
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // routes that don't require authentication
   publicRoutes: [
     "/terms-conditions",
-   "/signup(.*)",
-   "/signin(.*)",
-   "/register",
-"/demo",
-"/sites(.*)",
-"/site(.*)",
-"/home(.*)",
-"/post(.*)",
-"/welcome",
- ,  "/auth",
-   "/sign-up(.*)",
- "/sign-in(.*)",   
- "/login",
- "/dashboard(.*)",
- "/p(.*)",
- "/"
+    "/signup(.*)",
+    "/signin(.*)",
+    "/register",
+    "/demo",
+    "/sites(.*)",
+    "/site(.*)",
+    "/home(.*)",
+    "/post(.*)",
+    "/welcome",
+    "/auth",
+    "/sign-up(.*)",
+    "/sign-in(.*)",
+    "/login",
+    "/dashboard(.*)",
+    "/p(.*)",
+    "/"
   ],
 
   afterAuth(auth, req) {
     if (auth.isPublicRoute) {
       // do nothing for public routes
-      return NextResponse.next()
+      return;
     }
 
-    const url = new URL(req.nextUrl.origin)
+    const url = new URL(req.nextUrl.origin);
 
     if (!auth.userId && !auth.isPublicRoute) {
       // if the user tries to access a private route without being authenticated,
       // redirect to login page
-      url.pathname = "/login"
-      return NextResponse.redirect(url)
+      url.pathname = "/login";
+      return redirectToSignIn({ returnBackUrl: req.url });
     }
+
+    if (auth.userId && !auth.isPublicRoute) {
+      // if the user is logged in and trying to access a protected route,
+      // allow them to access the route
+      return;
+    }
+
+    // All other cases (e.g., not logged in but accessing a public route)
+    // can be handled as needed
   },
-})
+});
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
